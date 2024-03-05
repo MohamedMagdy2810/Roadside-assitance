edb70ed1d99c764c23ec641238fa0418eb346a53
Mohamed Magdy <mohamedgezar20@gmail.com>
2024-03-05 02:48:47 +0200

    add a custom padding to the bottom sheet

--- lib/widgets/bottom_sheet.dart
+++ lib/widgets/bottom_sheet.dart
@@ -9,19 +9,22 @@ class bottoSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 400,
+    
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal:16.0),
-        child: Column(
-          children: [
-            SizedBox(height: 32,),
-            CustomTextField(hintText: 'Enter servcie type'),
-            SizedBox(height: 32,),
-            CustomTextField(hintText: 'Descripton',maxlines: 5,),
-            SizedBox(height: 32,),
-            customButton(text: 'Send request')
-        
-          ],
+        padding:  EdgeInsets.only(right:16.0,left: 16,bottom: MediaQuery.of(context).viewInsets.bottom),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: 32,),
+              CustomTextField(hintText: 'Enter servcie type'),
+              SizedBox(height: 32,),
+              CustomTextField(hintText: 'Descripton',maxlines: 5,),
+              SizedBox(height: 32,),
+              customButton(text: 'Send request'),
+              SizedBox(height: 32,),
+          
+            ],
+          ),
         ),
       ),
     );
