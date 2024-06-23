3bc2aacdd0e280eb71d236e30fe818b82f16d32a
Mohamed Magdy <mohamedgezar20@gmail.com>
2024-06-23 18:56:00 +0300

    fix issue with whatsapp

--- lib/models/data_response_model.dart
+++ lib/models/data_response_model.dart
@@ -1,10 +1,10 @@
 class response_model {
   final List<ServiceProvider> serviceProviders;
-  final List<User> users;
+  
 
   response_model({
     required this.serviceProviders,
-    required this.users,
+    
   });
 
   factory response_model.fromJson(Map<String, dynamic> jsonData) {
@@ -15,34 +15,13 @@ class response_model {
         .map((item) => ServiceProvider.fromJson(item))
         .toList();
 
-    List<User> usersList = usersJson.map((item) => User.fromJson(item)).toList();
-
     return response_model(
       serviceProviders: serviceProvidersList,
-      users: usersList,
+  
     );
   }
 }
 
-class User {
-  final String username;
-  final String? phoneNumber;
-  final List<double> location;
-
-  User({
-    required this.username,
-    required this.phoneNumber,
-    required this.location,
-  });
-
-  factory User.fromJson(Map<String, dynamic> jsonData) {
-    return User(
-      username: jsonData['username'],
-      phoneNumber: jsonData['phone_number'],
-      location: List<double>.from(jsonData['location']),
-    );
-  }
-}
 
 class ServiceProvider {
   final String username;
